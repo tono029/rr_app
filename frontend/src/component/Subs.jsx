2725a8987d3352359073f6c53ac4d3478ada5e76
Tono <tonoyama0229@gmail.com>
2022-04-09 15:20:56 +0900

    link modal

--- frontend/src/component/Subs.jsx
+++ frontend/src/component/Subs.jsx
@@ -8,7 +8,7 @@ import IconButton from '@mui/material/IconButton';
 import { useForm } from "react-hook-form";
 
 export default function Subs(props) {
-  const [showModal, setShowModal] = React.useState(false)
+  const [open, setOpen] = React.useState([false, ""])
 
   async function deleteSub(id) {
     await props.client.delete(`subs/${id}`)
@@ -25,12 +25,13 @@ export default function Subs(props) {
     console.log("data", data)
 
     // rails側に送信
-    // updateSub(data, id)
-    // props.getSubs()
-    setShowModal(false)
+    updateSub(data, open[1])
+    props.getSubs()
+    reset()
+    setOpen([false, ""])
   }
 
-  const {register, handleSubmit} = useForm({
+  const {register, handleSubmit, reset} = useForm({
     mode: onSubmit,
     defaultValues: {link: ""},
 
@@ -45,32 +46,28 @@ export default function Subs(props) {
     bgcolor: 'white',
     border: "none",
     borderRadius: "10px",
-    boxShadow: "0px 5px 15px rgba(0,0,0,0.1)",
+    boxShadow: "0px 5px 15px rgba(0,0,0,0.3)",
     p: 4,
     padding: "0px 30px 20px 30px",
   }
   
-  function openLink(url) {
-    window.open(url)
-  }
 
   const subsIndex = props.subs.map(sub => {
     return (
       <TableRow key={sub.id}>
         {/* クリックで編集できるように */}
-        <TableCell>{sub.sub_name}, {sub.id}</TableCell>
+        <TableCell>{sub.sub_name}</TableCell>
 
         <TableCell>{sub.fee.toLocaleString()}<span>円</span></TableCell>
 
         <TableCell>{sub.period === 1 ? "/月" : "/年"}</TableCell>
 
-        {/* リンクが設定されているかで分岐、なければonClickでmodal表示。 */}
         {
           sub.link !== "" ?
           <TableCell>
             <IconButton 
               className="link-btn"
-              onClick={() => openLink(sub.link)}  
+              onClick={() => window.open(sub.link)}  
             >
               <LinkIcon />
             </IconButton>
@@ -79,6 +76,7 @@ export default function Subs(props) {
           <TableCell>
             <IconButton 
               className="link-btn"
+              onClick={() => setOpen([true, sub.id])}
             >
               <AddLinkIcon />
             </IconButton>
@@ -95,6 +93,30 @@ export default function Subs(props) {
             ×
           </Button>
         </TableCell>
+
+        <Modal
+          // openがtrueかつ、idが一致するモーダルを開く。
+          open={open[0] && open[1] === sub.id}
+          onClose={() => setOpen([false, ""])}
+        >
+          <Box sx={modalStyle}>
+            <p>リンク先を追加</p>
+            <div className="modal-form">
+              <TextField
+                label="リンク"
+                size="small"
+                {...register("link")}
+              />
+              
+              <Button
+                variant="contained"
+                onClick={handleSubmit(onSubmit)}
+              >
+                追加
+              </Button>
+            </div>
+          </Box>
+        </Modal>
       </TableRow>
     )
   })
