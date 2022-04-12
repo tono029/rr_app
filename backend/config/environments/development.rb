460c5e20c0758e225e5b7209abcafca821e9d884
Tono <tonoyama0229@gmail.com>
2022-04-12 19:45:53 +0900

    mailer

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -63,4 +63,20 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.default_options = { from: ENV['EMAIL_ADDRESS'] }
+  # hostにはデフォルトでlocalhost3000になっているので、Railsのポート番号である3001に変更する。
+  config.action_mailer.default_url_options = { host: 'localhost:3001' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['EMAIL_ADDRESS'],
+    password: ENV['EMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
+  
 end
