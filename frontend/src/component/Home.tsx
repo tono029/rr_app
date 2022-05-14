b999beeacba8874da32dbec07455c871daa25738
Tono <tonoyama0229@gmail.com>
2022-05-14 21:04:21 +0900

    layout

--- frontend/src/component/Home.tsx
+++ frontend/src/component/Home.tsx
@@ -4,16 +4,20 @@ export default function Home() {
   return (
     <div className="home-container">
       <div className="home-text">
-        <h2>SubscManagerは利用しているサブスクリプションサービスを登録、管理する機能を提供します。</h2>
+        <h2>
+          <span>SubscManagerは利用している</span>
+          <br></br>
+          サブスクリプションサービス<span>を</span>登録、管理
+          <br></br>
+          <span>する機能を提供します。</span>
+        </h2>
 
         <p>
           利用しているサービスの月々の料金把握やリストによる管理、グラフによる可視化が可能です。
         </p>
       </div>
 
-      <div className="img-container">
-        <img src={sample} alt="" />
-      </div>
+        <img src={sample} alt="" className="img-container"/>
     </div>
   )
 }
